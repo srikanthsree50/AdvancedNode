2ee403b297316f0f7c174c407a5892d203063b0f
srikanthsree50 <srikanthsree50@gmail.com>
2025-03-26 19:42:36 +0530

    fixing errors

--- async.js
+++
@@ -1,9 +0,0 @@
-const https = require('https');
-const start = Date.now();
-process.env.UV_THREADPOOL_SIZE = 5
-https.request('https://www.google.com', res =>{
-    res.on('data',() => {});
-    res.on('end',() => {
-        console.log(Date.now() - start);
-    })
-}).end();
